7b61a787a936247656b2f3af362eed4430ed1031
vicky <victoria.awuor@student.moringaschool.com>
2022-04-19 21:49:01 +0300

    created the model classes

--- Housingapp/models.py
+++ Housingapp/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
+from phone_field import PhoneField
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+#create models
+class Landlord(models.Model):
+    uname = models.TextField(max_length=100)
+    contact = PhoneField(null=True, blank=True)
+    email = models.EmailField(null=True, blank=True)
+    post = CloudinaryField('images', null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE,null = True, related_name='user')
+    
+    def __str__(self):
+        return f'{self.uname} Landlord'
+
+class Tenant(models.Model):
+    name = models.TextField(max_length=100)
+    contact = PhoneField(null=True, blank=True)
+    email = models.EmailField(null=True, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE,null = True, related_name='user')
+    landlord = models.ForeignKey(Landlord, on_delete=models.CASCADE,null = True, related_name='landlord')
+
+    def __str__(self):
+        return f'{self.name} Tenant'
